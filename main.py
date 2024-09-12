3dafa09f3552dcfa6f8031a462a99fc3b79bb7d1
EdKabanchik777 <1103erg@mail.ru>
2024-09-12 19:25:39 +0500

    создание склада

--- main.py
+++ main.py
@@ -1,11 +1,43 @@
-class Home:
-    def __init__(self, floors, rooms):
-        self.floors = floors
-        self.rooms = rooms
+class Warehouse:
+    translations = {
+        'beef': 'говядины, ',
+        'pork': 'свинины, ',
+        'mutton': 'баранины, '
+    }
 
-    def color_room (self, room_number):
-        pass
+    beef = 0
+    pork = 0
+    mutton = 0
 
-class Car:
-    def __init__(self):
-        pass
+    def add_meat(self, meat, weight):
+        print('Приняли: ', self.translations[meat], 'вес: ', weight)
+        old_weight = self.get_meat(meat)
+        new_weight = old_weight + weight
+        setattr(self, meat, new_weight)
+        self.print_remains(meat)
+
+    def remove_meat(self, meat, weight):
+        print('Отпустили: ', self.translations[meat], 'вес: ', weight)
+        old_weight = self.get_meat(meat)
+        new_weight = old_weight - weight
+        setattr(self, meat, new_weight)
+        self.print_remains(meat)
+
+    def get_meat(self, meat):
+        return getattr(self, meat)
+
+    def print_remains(self, meat):
+        print('Остатки', self.translations[meat], self.get_meat(meat))
+
+
+
+my_sklad = Warehouse()
+my_sklad.add_meat('beef', 100)
+my_sklad.add_meat('pork', 230)
+my_sklad.add_meat('mutton', 10)
+my_sklad.add_meat('mutton', 35)
+my_sklad.remove_meat('pork', 33)
+
+
+
+
